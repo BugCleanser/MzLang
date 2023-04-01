e700689de3091b6f25da0096d29329182dd4bb97
mz <2323346933@qq.com>
2023-04-01 21:30:54 +0800

    Update MzPrimitiveInterface.java

--- src/mz/mzlang/loader/MzPrimitiveInterface.java
+++ src/mz/mzlang/loader/MzPrimitiveInterface.java
@@ -1,5 +1,7 @@
 package mz.mzlang.loader;
 
+import java.util.*;
+
 public class MzPrimitiveInterface extends MzInterface
 {
 	public static MzPrimitiveInterface BOOL=new MzPrimitiveInterface();
@@ -10,6 +12,19 @@ public class MzPrimitiveInterface extends MzInterface
 	public static MzPrimitiveInterface LONG=new MzPrimitiveInterface();
 	public static MzPrimitiveInterface FLOAT=new MzPrimitiveInterface();
 	public static MzPrimitiveInterface DOUBLE=new MzPrimitiveInterface();
+	public static Map<String,MzPrimitiveInterface> values;
+	static
+	{
+		values=new HashMap<>();
+		values.put("Bool",BOOL);
+		values.put("Char",CHAR);
+		values.put("Byte",BYTE);
+		values.put("Short",SHORT);
+		values.put("Int",INT);
+		values.put("Long",LONG);
+		values.put("Float",FLOAT);
+		values.put("Double",DOUBLE);
+	}
 	
 	@Override
 	public boolean equals(Object o)
